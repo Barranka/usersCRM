7ecfeb61453214c42376f9b0150a7a5673880d11
Barranka <cat123_93@mail.ru>
2020-07-13 13:13:18 +0300

    first commit

---
+++ src/routers.js
@@ -0,0 +1,26 @@
+//default
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+//Pages
+import NotFound from '@/pages/404'
+import Users from '@/pages/Users'
+
+//Routering
+export default new Router({
+	mode: 'history',
+	routes: [
+		{
+			path: '/',
+			name: 'users',
+			component: Users
+		},
+		{
+			path: '*',
+			name: 'notFound',
+			component: NotFound
+		}
+	]
+})
