1b70eedac853ac135d3f4a7a41118d393bfdf994
Barranka <cat123_93@mail.ru>
2020-07-13 15:04:43 +0300

    first commit

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,10 @@
 // vue.config.js
 module.exports = {
 	publicPath: process.env.NODE_ENV === 'production'
-   		? '/'
+   		? './'
 		: '/',
 	assetsDir: '',
 	productionSourceMap: false,
-	filenameHashing: false
+	outputDir: 'docs',
+	//filenameHashing: false
   }
